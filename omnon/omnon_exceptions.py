00321a81a20a0ea521a0f874163a6bf3973f6bcd
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-09-28 05:13:09 +0200

    Load image and generate puzzle pieces

--- omnon/omnon_exceptions.py
+++
@@ -1,3 +0,0 @@
-class Glutton(Exception):
-    def __init__(self, carried_piece, new_piece):
-        super().__init__("Someone's hungry. Attempted to bite piece at {carried_piece.pos} while carrying piece from {new_piece.pos}")
