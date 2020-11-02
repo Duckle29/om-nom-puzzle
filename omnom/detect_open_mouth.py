343b10577e94d5a65d8e2c6913232f268465f45e
MichaelBidstrup <43850522+MichaelBidstrup@users.noreply.github.com>
2020-11-02 17:07:41 +0100

    Added eyebrow reset

--- omnom/detect_open_mouth.py
+++ omnom/detect_open_mouth.py
@@ -16,6 +16,8 @@ class MahMouth():
 	MOUTH_AR_THRESH = 0.79
 	# grab the indexes of the facial landmarks for the mouth
 	(mStart, mEnd) = (49, 68)
+	(bStart, bEnd) = (18,27)
+	(eStart, eEnd) = (37, 48)
 
 	def __init__(self):
 		self.construct_arguments()
@@ -26,21 +28,6 @@ class MahMouth():
 		# do a bit of cleanup
 		cv2.destroyAllWindows()
 		vs.stop()
-
-	def mouth_aspect_ratio(self, mouth):
-		# compute the euclidean distances between the two sets of
-		# vertical mouth landmarks (x, y)-coordinates
-		A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
-		B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
-		# compute the euclidean distance between the horizontal
-		# mouth landmark (x, y)-coordinates
-		C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
-
-		# compute the mouth aspect ratio
-		mar = (A + B) / (2.0 * C)
-
-		# return the mouth aspect ratio
-		return mar
 
 	def construct_arguments(self):
 			# construct the argument parse and parse the arguments
@@ -65,6 +52,35 @@ class MahMouth():
 
 		time.sleep(1.0)	
 
+	def mouth_aspect_ratio(self, mouth):
+		# compute the euclidean distances between the two sets of
+		# vertical mouth landmarks (x, y)-coordinates
+		A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
+		B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
+		# compute the euclidean distance between the horizontal
+		# mouth landmark (x, y)-coordinates
+		C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
+
+		# compute the mouth aspect ratio
+		mar = (A + B) / (2.0 * C)
+
+		# return the mouth aspect ratio
+		return mar
+
+	def brow_movement(self, brows, eyes):
+		left = dist.euclidean(brows[3], eyes[2]) # 21, 39
+		right = dist.euclidean(brows[6], eyes[7]) # 24, 44
+		eye_dist = dist.euclidean(eyes[3], eyes[6]) #40, 43
+
+		diff = (left + right) / (2.0 * eye_dist)
+		if diff > 0.5:
+			reset = True
+		else:
+			reset = False
+
+		print(reset)
+		return reset
+
 	def update(self):
 
 		frame = self.vs.read()
@@ -84,24 +100,42 @@ class MahMouth():
 			# extract the mouth coordinates, then use the
 			# coordinates to compute the mouth aspect ratio
 			mouth = shape[self.mStart:self.mEnd]
+			brows = shape[self.bStart:self.bEnd]
+			eyes = shape[self.eStart:self.eEnd]
 
-			mouthMAR = self.mouth_aspect_ratio(mouth)
-			mar = mouthMAR
+			mouthAR = self.mouth_aspect_ratio(mouth)
+			browsAR = self.brow_movement(brows, eyes)
+
+			right_eye = shape[37:42]
+			left_eye = shape[43:46]
+
 			# compute the convex hull for the mouth, then
 			# visualize the mouth
 			mouthHull = cv2.convexHull(mouth)
+			left_eyeHull = cv2.convexHull(left_eye)
+			right_eyeHull = cv2.convexHull(right_eye)
 			
-			cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 2)
-			cv2.putText(frame, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+			#cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 2)
+			#cv2.drawContours(frame, [left_eyeHull], -1, (0, 255, 0), 1)
+			#cv2.drawContours(frame, [right_eyeHull], -1, (0, 255, 0), 1)
+			cv2.putText(frame, "MAR: {:.2f}".format(mouthAR), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+			for (x, y) in shape:
+				cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
 			# Draw text if mouth is open
-			if mar > mouthcheck.MOUTH_AR_THRESH:
+			if mouthAR > mouthcheck.MOUTH_AR_THRESH:
 				isopen = True
 				cv2.putText(frame, "Mouth is Open!", (30,60),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
 			else:
 				isopen = False
 				
+					# Draw text if mouth is open
+			if browsMOVED == True:
+				cv2.putText(frame, "Reset!", (30,90),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+
 		# show the frame
 		cv2.imshow("Frame", frame)
 
@@ -112,7 +146,7 @@ if __name__ == "__main__":
 
 	while True:
 		mouthopen = mouthcheck.update()
-		print(mouthopen)
+		#print(mouthopen)
 		# if the `q` key was pressed, break from the loop
 		key = cv2.waitKey(1) & 0xFF
 		if key == ord("q"):
